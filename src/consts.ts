508b2aae83f7cf13cd79853c2bb4161dc67a303e
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2025-12-26 16:56:57 +0200

    feat: Update site title and description for improved branding and SEO, and enhance metadata in BaseHead components

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'MS | Blog';
-export const SITE_DESCRIPTION = 'A software developer sharing insights on backend, frontend, and architecture through real-world development experiences.';
+export const SITE_TITLE = 'Mahmoud Saeed | Blog';
+export const SITE_DESCRIPTION = 'Mahmoud Saeed\'s blog: Insights on software engineering, backend development, frontend technologies, system architecture, and real-world programming experiences.';
