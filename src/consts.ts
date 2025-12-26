4d98b9aadd37fe0bedfd64557d244105c93482f7
Mahmoud-Saeed-Mahmoud <51866708+Mahmoud-Saeed-Mahmoud@users.noreply.github.com>
2025-12-26 16:36:39 +0200

    docs: Update site description and hero section role from 'Software Engineer' to 'Software Developer', and apply minor formatting adjustments.

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Mahmoud Saeed';
-export const SITE_DESCRIPTION = 'A software engineer sharing insights on backend, frontend, and architecture through real-world development experiences.';
+export const SITE_DESCRIPTION = 'A software developer sharing insights on backend, frontend, and architecture through real-world development experiences.';
